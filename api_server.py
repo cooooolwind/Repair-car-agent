a185ceadbbd6727c2bd2e8ec278bf6a8ee10cb66
Aria Suko <Ariadnesuko@hotmail.com>
2026-01-30 17:28:57 +0800

    修改监听地址为0.0.0.0以允许局域网访问

--- api_server.py
+++ api_server.py
@@ -131,7 +131,7 @@ async def get_image(filename: str):
 if __name__ == "__main__":
     uvicorn.run(
         app,
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         log_level="info"
     )
